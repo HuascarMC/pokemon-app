2a33f5631d0f6bef8b1a20524a2ece6be3d0180e
Huascar <huascarmc@hotmail.com>
2020-02-01 22:08:10 -0400

    +pokedex design

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -23,7 +23,7 @@ export class PokemonService {
   }
 
   getPokemons(): Observable<Pokemon[]> {
-    return this.http.get<Pokemon[]>("https://pokeapi.co/api/v2/pokemon/")
+    return this.http.get<Pokemon[]>("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10")
     .pipe(
      catchError(this.handleError)
     );
